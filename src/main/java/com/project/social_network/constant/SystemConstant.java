f650bcad11538d2c24e4251fc9c8903b08c9f7bb
Nguyen Duy <bogiaoffline@gmail.com>
2025-03-02 21:18:42 +0700

    ul src code

---
+++ src/main/java/com/project/social_network/constant/SystemConstant.java
@@ -0,0 +1,15 @@
+package com.project.social_network.constant;
+
+public class SystemConstant {
+
+  public static final String SUCCESS = "success";
+  public static final String ERROR = "error";
+  public static final String INTERNAL_SERVER_ERROR = "Lỗi hệ thống!";
+
+  public static final String BAD_REQUEST = "Yêu cầu không hợp lệ!";
+  public static final String UNAUTHORIZED = "Bạn chưa đăng nhập!";
+  public static final String FORBIDDEN = "Bạn không có quyền truy cập!";
+  public static final String NOT_FOUND = "Không tìm thấy tài nguyên!";
+
+
+}
