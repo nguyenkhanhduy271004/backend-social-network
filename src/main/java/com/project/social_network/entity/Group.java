5bf73f8c539cd0ae3b396823687e22e4520c5a60
Nguyen Duy <bogiaoffline@gmail.com>
2025-03-15 22:22:47 +0700

    ul src code

--- src/main/java/com/project/social_network/entity/Group.java
+++ src/main/java/com/project/social_network/entity/Group.java
@@ -6,6 +6,7 @@ import jakarta.persistence.ManyToMany;
 import jakarta.persistence.OneToMany;
 import java.util.ArrayList;
 import java.util.List;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -14,6 +15,7 @@ import lombok.Setter;
 @Getter
 @Setter
 @NoArgsConstructor
+@AllArgsConstructor
 public class Group extends BaseEntity{
 
   private String name;
